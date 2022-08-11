5e243746db71680b364f34b446e1ef03d6a3d75a
JheimisonFLX <jheimison_007@hotmail.com>
2022-08-11 10:30:21 -0400

    06 Detalhes da cesta

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import { StatusBar, SafeAreaView } from 'react-native';
-import Cesta from './src/telas/Cesta';
-
+import { StatusBar, SafeAreaView } from "react-native";
+import Cesta from "./src/telas/Cesta";
 
 export default function App() {
   return (
     <SafeAreaView>
-      <StatusBar/>
-     <Cesta/>
+      <StatusBar />
+      <Cesta />
     </SafeAreaView>
   );
 }
-
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Text,Image, StyleSheet, Dimensions } from "react-native";
+import { Text, Image, StyleSheet, Dimensions, View } from "react-native";
 
 import topo from "../../assets/topo.png";
+import logo from "../../assets/logo.png";
 
 const width = Dimensions.get("screen").width;
 
@@ -10,6 +11,20 @@ export default function Cesta() {
     <>
       <Image source={topo} style={estilos.topo} />
       <Text style={estilos.titulo}>Detalhe da cesta</Text>
+
+      <View style={estilos.cesta}>
+        <Text style={estilos.nome}>Cesta de Verduras</Text>
+        <View style={estilos.fazenda}>
+          <Image source={logo} style={estilos.imagemFazenda} />
+          <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        </View>
+        <Text style={estilos.descricao}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500.
+        </Text>
+        <Text style={estilos.preco}>R$ 40,00</Text>
+      </View>
     </>
   );
 }
@@ -19,14 +34,61 @@ const estilos = StyleSheet.create({
     width: "100%",
     height: (578 / 768) * width,
   },
-  titulo:{
-    position:"absolute",
+
+  //#region Estilos
+
+  titulo: {
+    position: "absolute",
     width: "100%",
     textAlign: "center",
     fontSize: 16,
-    lineHeight:26,
+    lineHeight: 26,
     color: "white",
     fontWeight: "bold",
-    padding: 16
-  }
+    padding: 16,
+  },
+
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: "bold",
+  },
+
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12
+  },
+
+  imagemFazenda: {
+    width: 32,
+    height: 32    
+  },
+
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12
+  },
+
+  descricao: {
+    color: "#a3a3a3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+
+  preco: {
+    color: "#2a9f85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+  },
+
+  //#endregion
 });
